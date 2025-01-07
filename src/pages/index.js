dc6f6b00d567436414665820366bd5416b17cc50
Эркин Урманов <152743282+Erkin33@users.noreply.github.com>
2025-01-07 15:58:09 +0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,8 +14,8 @@ import Footer from "@/components/Footer";
 import Slider from "@/components/Slider";
 const products = [
   { id: 'one', name: 'Romantic Florals',sell: '-15% Off',description: 'Wrap Maxi Dress Blush', price: 39, image: '/Shorts/Black.webp' },
-  { id: 'two', name: 'Romantic Florals',sell: 'Out Of Stock',description: 'Wrap Maxi Dress Blush', price: 39, image: '/Shorts/Black2.webp' },
-  { id: 'three', name: 'Romantic Florals',sell: 'New',description: 'Wrap Maxi Dress Blush', price: 39, image: '/Shorts/Black3.webp' },
+  { id: 'two', name: 'Romantic Florals',sell: 'Out Of Stock',description: 'Wrap Maxi Dress Blush', price: 39, image: '/Shorts/black2.webp' },
+  { id: 'three', name: 'Romantic Florals',sell: 'New',description: 'Wrap Maxi Dress Blush', price: 39, image: '/Shorts/black3.webp' },
   { id: 'four', name: 'Romantic Florals',sell: 'New',description: 'Wrap Maxi Dress Blush', price: 39, image: '/Shorts/blue.jpg' },
 ];
 
